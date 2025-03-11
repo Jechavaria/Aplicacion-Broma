e69a01d9e8cf5f38d6b4511ca316115e55403963
Juan José Echavarria Castaño <juanjoseechavarriacastano@gmail.com>
2025-03-11 13:20:34 -0500

    Agregar archivos de proyecto.

---
+++ Aplicacion Broma/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace Aplicacion_Broma
+{
+    partial class frmFormatearEquipo
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAceptar = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.lbl = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnAceptar
+            // 
+            this.btnAceptar.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAceptar.Location = new System.Drawing.Point(316, 337);
+            this.btnAceptar.Name = "btnAceptar";
+            this.btnAceptar.Size = new System.Drawing.Size(147, 56);
+            this.btnAceptar.TabIndex = 0;
+            this.btnAceptar.Text = "Aceptar";
+            this.btnAceptar.UseVisualStyleBackColor = true;
+            this.btnAceptar.Click += new System.EventHandler(this.btnAceptar_Click);
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancelar.Location = new System.Drawing.Point(614, 337);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(160, 56);
+            this.btnCancelar.TabIndex = 1;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            this.btnCancelar.MouseHover += new System.EventHandler(this.btnCancelar_MouseHover);
+            // 
+            // lbl
+            // 
+            this.lbl.AutoSize = true;
+            this.lbl.BackColor = System.Drawing.Color.Transparent;
+            this.lbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl.Location = new System.Drawing.Point(285, 132);
+            this.lbl.Name = "lbl";
+            this.lbl.Size = new System.Drawing.Size(503, 38);
+            this.lbl.TabIndex = 2;
+            this.lbl.Text = "¿Desea borrar todos los datos?";
+            // 
+            // frmFormatearEquipo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1282, 703);
+            this.Controls.Add(this.lbl);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnAceptar);
+            this.Name = "frmFormatearEquipo";
+            this.Text = "Formatea equipo";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAceptar;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Label lbl;
+    }
+}
+
---
+++ Aplicacion Broma/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Aplicacion_Broma
+{
+    public partial class frmFormatearEquipo : Form
+    {
+        private Random rnd = new Random();
+
+        public frmFormatearEquipo()
+        {
+            InitializeComponent(); 
+        }
+
+
+        private void btnCancelar_MouseHover(object sender, EventArgs e)
+        {
+            btnCancelar.Location = new Point(
+                rnd.Next(0, this.ClientSize.Width - btnCancelar.Width),
+                rnd.Next(0, this.ClientSize.Height - btnCancelar.Height)
+            );
+        }
+
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            btnCancelar.Location = new Point(
+                rnd.Next(0, this.ClientSize.Width - btnCancelar.Width),
+                rnd.Next(0, this.ClientSize.Height - btnCancelar.Height)
+            );
+        }
+
+
+        private void btnAceptar_Click(object sender, EventArgs e)
+        {
+            FancyMessageBox.ShowFancy("Formateando equipo...");
+            this.Close();
+        }
+    }
+
+    public class FancyMessageBox : Form
+    {
+        public FancyMessageBox(string message)
+        {
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.Size = new Size(300, 150);
+            this.BackColor = Color.FromArgb(30, 30, 30);
+
+
+            Label lblMessage = new Label
+            {
+                Text = message,
+                ForeColor = Color.White,
+                Font = new Font("Arial", 12, FontStyle.Bold),
+                TextAlign = ContentAlignment.MiddleCenter,
+                AutoSize = false,
+                Width = this.ClientSize.Width,
+                Height = 60,
+                Location = new Point(0, 0)
+            };
+
+            Button btnClose = new Button
+            {
+                Text = "Cerrar",
+                Font = new Font("Arial", 10, FontStyle.Bold),
+                ForeColor = Color.White,
+                BackColor = Color.FromArgb(50, 50, 50),
+                FlatStyle = FlatStyle.Flat,
+                Size = new Size(80, 30),
+                Location = new Point((this.ClientSize.Width - 80) / 2, 80)
+            };
+            btnClose.Click += (s, e) => this.Close();
+
+            this.Controls.Add(lblMessage);
+            this.Controls.Add(btnClose);
+
+
+            this.Opacity = 0;
+            Timer timer = new Timer { Interval = 50 };
+            timer.Tick += (s, e) =>
+            {
+                if (this.Opacity < 1)
+                    this.Opacity += 0.1;
+                else
+                    timer.Stop();
+            };
+            timer.Start();
+        }
+
+
+        public static void ShowFancy(string message)
+        {
+
+            FancyMessageBox box = new FancyMessageBox(message)
+            {
+                StartPosition = FormStartPosition.CenterScreen
+            };
+
+            box.ShowDialog();
+        }
+    }
+
+}
